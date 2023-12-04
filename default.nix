37da0fae2665431330892d8f97b9c7dbd01d5858
LiquidZulu <liquidzulu@pm.me>
2023-12-04 18:03:05 +0000

    recursively create directory
    Previously it would error on Windows because parent directories did not exist (instead of `$XDG_CONFIG_HOME/pompom` it would try to create `%appdata%/LiquidZulu/pompom`).

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "pompom";
-  buildInputs = [ pkg-config alsa-lib ];
+  buildInputs = [ pkg-config alsa-lib libudev-zero libinput ]
+    ++ (with xorg; [ libX11 libXtst ]);
 }
